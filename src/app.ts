43f5257762ae7c5970df1e6fec8af73195582e7a
rvaldez20 <rvaldez20@gmail.com>
2024-02-08 21:47:10 -0600

    [ADD] se crea una funcion asincrona auto invocada(para hacer uso de async/await)

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,18 @@
 //! usando pakage yargs
 import { yarg } from "./config/plugins/yargs.plugin";
 
-console.log(process.argv)
-
-console.log(yarg);
-
-
-
-
-
-
-//! usando argv the node
 // console.log(process.argv)
-// const [tsnode, app, ...args] = process.argv;
-// console.log(args)
+// console.log('=============================')
+// console.log(yarg.b);
+
+
+(async() => {
+   await main();
+})();
+
+async function main() {
+
+
+
+}
 
