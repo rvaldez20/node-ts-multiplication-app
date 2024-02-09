985c5d30f10559282bb41dc412984c96805e471c
rvaldez20 <rvaldez20@gmail.com>
2024-02-08 19:23:05 -0600

    [ADD] my version create table multiplication

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,35 @@
+import fs from 'fs';
+import path from 'path';
 
-const message: string = 'Hola Mundo!';
-console.log(message);
+const createTableNumber = (numberTable: number) => {
+
+   // const numberTable: number = 5;
+   const archivo = path.join(__dirname, `outputs/tabla-${numberTable}.txt`);
+
+
+   let myData = '';
+   console.log('=============================')
+   console.log(`        Table ${numberTable}`)
+   console.log('=============================')
+
+   myData += '=============================\n';
+   myData += `          Table ${numberTable}\n`
+   myData += '=============================\n';
+
+   try {
+
+      for (let i = 1; i <= 10; i++) {
+         console.log(`${numberTable} X ${i} = ${numberTable * i}`);
+         myData += `${numberTable} X ${i} = ${numberTable * i}\n`;
+         fs.writeFileSync(archivo, myData);
+      }
+
+   } catch (error) {
+      console.log(error);
+   }
+}
+
+
+createTableNumber(6);
+
 
