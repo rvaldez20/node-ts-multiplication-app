debc8891735e1013cdb73c17cb70c80016edff0c
rvaldez20 <rvaldez20@gmail.com>
2024-02-08 21:32:20 -0600

    [ADD] introduccion a yargs, first steps

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,17 @@
-
+//! usando pakage yargs
+import { yarg } from "./config/plugins/yargs.plugin";
 
 console.log(process.argv)
 
-const [tsnode, app, ...args] = process.argv;
-console.log(args)
+console.log(yarg);
+
+
+
+
+
+
+//! usando argv the node
+// console.log(process.argv)
+// const [tsnode, app, ...args] = process.argv;
+// console.log(args)
+
