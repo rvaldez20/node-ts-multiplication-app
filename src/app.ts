4375a174316ae5ec574d771713039a30ab11e9ab
rvaldez20 <rvaldez20@gmail.com>
2024-02-09 21:14:24 -0600

    [ADD] iniciando refactorizacion clean arquitecture
    => creamos file system: dir presentation
    => app.ts es el punto de entrada d ela aplicaición
    => usamos POO creando la clase ServerApp
    => creamos el method static run()
    => se le pasa los args de yarg
    => se creo una interface para las options: base, limit y show

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,20 @@
 //! usando pakage yargs
 import { yarg } from "./config/plugins/args.plugin";
-
-
-// console.log(process.argv)
-// console.log('=============================')
-// console.log(yarg.b);
+import { ServerApp } from "./presentation/server-app";
 
 
 (async() => {
    await main();
 })();
 
+
+
 async function main() {
 
-   console.log( yarg )
+   const { b:base, l:limit, s:showTable } = yarg;
+   // console.log(yarg);
+
+   ServerApp.run({ base, limit, showTable });
 
 }
 
---
+++ src/presentation/server-app.ts
@@ -0,0 +1,20 @@
+
+interface RunOptions {
+   base: number;
+   limit: number;
+   showTable: boolean;
+}
+
+
+export class ServerApp {
+
+
+
+   //* Methods
+   static run(options: RunOptions){
+
+      console.log('Server running...');
+      console.log(options);
+   }
+
+}
